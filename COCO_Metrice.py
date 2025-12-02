1f47ebba13392bfa8e9fdfaab0b8bc0b6ef96af9
UltralyticsAssistant <web@ultralytics.com>
2025-12-02 04:52:53 +0000

    Auto-format by https://ultralytics.com/actions

--- COCO_Metrice.py
+++ COCO_Metrice.py
@@ -1,7 +1,8 @@
 import warnings
 
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 import argparse
+
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 from tidecv import TIDE, datasets
@@ -9,20 +10,22 @@ from tidecv import TIDE, datasets
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--anno_json', type=str, default='data.json', help='label coco json path')
-    parser.add_argument('--pred_json', type=str, default='runs/detect/val4/predictions.json', help='pred coco json path')
+    parser.add_argument("--anno_json", type=str, default="data.json", help="label coco json path")
+    parser.add_argument(
+        "--pred_json", type=str, default="runs/detect/val4/predictions.json", help="pred coco json path"
+    )
 
     return parser.parse_known_args()[0]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     opt = parse_opt()
     anno_json = opt.anno_json
     pred_json = opt.pred_json
 
     anno = COCO(anno_json)  # init annotations api
     pred = anno.loadRes(pred_json)  # init predictions api
-    eval = COCOeval(anno, pred, 'bbox')
+    eval = COCOeval(anno, pred, "bbox")
     eval.evaluate()
     eval.accumulate()
     eval.summarize()
@@ -30,4 +33,4 @@ if __name__ == '__main__':
     tide = TIDE()
     tide.evaluate_range(datasets.COCO(anno_json), datasets.COCOResult(pred_json), mode=TIDE.BOX)
     tide.summarize()
-    tide.plot(out_dir='result-v10-BS2')
+    tide.plot(out_dir="result-v10-BS2")
